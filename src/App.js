347adbbac832413cb56d57c94ffbd62592e56b06
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2019-02-16 16:39:32 -0500

    Created Video component as a seperate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Videos from './components/Videos';
+
 class App extends Component {
 
   state = {
@@ -23,50 +25,10 @@ class App extends Component {
 
   render() {
 
-    const data =  this.state.data;
-    console.log(data, 'this is from wp');
-
-    const showData = data.map( ( item ) => {
-
-      const videoUrl = item.acf.videourl;
-
-      const getId = (url) => {
-        // Regular expresion gets URL video ID from URL
-        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-        const match = url.match(regExp);
-    
-        if (match && match[2].length == 11) {
-            return match[2];
-        } else {
-            return 'error';
-        }
-    }
-    
-    const videoId = getId(videoUrl);
-    const finalUrl = `https://www.youtube.com/embed/${videoId}`;
-    
-    const iframeMarkup = 
-      <iframe 
-        width="100%" 
-        height="300" 
-        src={finalUrl} 
-        frameBorder="0" 
-        allowFullScreen="0">
-      </iframe>;
-
-      return(
-        <div key={item.id}>
-          <h1>{item.title.rendered}</h1>
-          <div>{item.acf.position}</div>    
-          {iframeMarkup}
-        </div>
-      );
-    });
-
     return (
-      <div className="App-header">
+      <div className="app-body">
         <h1>My Videos</h1>
-        {showData}
+        <Videos data={this.state.data}/>
       </div>
     );
   }
